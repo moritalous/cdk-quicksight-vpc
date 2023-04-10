366302e1c6aca89fd0fe0bf50427a8a02089e156
moritalous <moritalous@gmail.com>
2023-04-10 11:30:15 +0000

    chore: project created with projen

---
+++ .projenrc.ts
@@ -0,0 +1,17 @@
+import { awscdk } from 'projen';
+const project = new awscdk.AwsCdkConstructLibrary({
+  author: 'moritalous',
+  authorAddress: 'moritalous@gmail.com',
+  cdkVersion: '2.1.0',
+  defaultReleaseBranch: 'main',
+  jsiiVersion: '~5.0.0',
+  name: 'cdk-quicksight-vpc',
+  projenrcTs: true,
+  repositoryUrl: 'https://github.com/moritalous/cdk-quicksight-vpc.git',
+
+  // deps: [],                /* Runtime dependencies of this module. */
+  // description: undefined,  /* The description is just a string that helps people understand the purpose of the package. */
+  // devDeps: [],             /* Build dependencies for this module. */
+  // packageName: undefined,  /* The "name" in package.json. */
+});
+project.synth();
